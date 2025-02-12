b3db9e362044602b844412386810870a470452c9
Jonathan Goiz Arana <Jonathanga3221@gmail.com>
2025-02-12 16:45:39 -0600

    Tipados

--- Estructurada/_2_MayorTres.py
+++ Estructurada/_2_MayorTres.py
@@ -1,15 +1,15 @@
 if __name__ == '__main__':
 
-a=int(input("introduce el valor de b "))
-b=int(input("introduce el valor de b "))
-c=int(input("introduce el valor de c "))
+ a=int(input("introduce el valor de b "))
+ b=int(input("introduce el valor de b "))
+ c=int(input("introduce el valor de c "))
 
-    if a>b
+if a>b:
     if a>c:
         print("el mayor es", a)
     else:
-        print("el mayor es", {b}")
-    elif b>c:
-        print (f"el mayor es {b}")
-    else:
-        print(f"el mayor es {c}")
+        print(f"el mayor es {b}")
+elif b>c:
+    print (f"el mayor es {b}")
+else:
+    print(f"el mayor es {c}")
---
+++ Estructurada/_3_TiposDeDatos/Tipados.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    edad: int =123456789012345678901234567890
+    nombre: str="Maria"
+    altura:float= 1.1234567890123456789
+    activo:bool=True
+
+    print(edad)
+    print(nombre)
+    print(altura)
+    print(activo)
