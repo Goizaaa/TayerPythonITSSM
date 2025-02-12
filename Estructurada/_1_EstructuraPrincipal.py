50d1d5e17d1d1a7669ef4a7c2cdbef4fcd058e4a
Jonathan Goiz Arana <Jonathanga3221@gmail.com>
2025-02-12 15:52:51 -0600

    cse incluye el cubo de suma

--- Estructurada/_1_EstructuraPrincipal.py
+++ Estructurada/_1_EstructuraPrincipal.py
@@ -2,5 +2,8 @@ if __name__ == '__main__':
     a:int=10
     b:int=50
     suma:int=a+b
+    cubo= suma*suma*suma
 
-    print(f"{suma}")
+    print(f"la suma es{suma}")
+    print(f"la cubo de esa {suma} es: {cubo}")
+
