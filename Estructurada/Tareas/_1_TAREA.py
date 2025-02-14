86048275acf7e1630950ab62e1e4131f4be553da
MSC Tomás González <tomas.gonzalez@smartin.tecnm.mx>
2025-02-14 05:47:56 -0600

    Indicaciones tarea 1

--- Estructurada/Tareas/_1_TAREA.py
+++ Estructurada/Tareas/_1_TAREA.py
@@ -1,1 +1,9 @@
-#utilizar un for para imprimir cada uni de los elementos de los tipos de datps de collections
+#Tarea 12 de Febrero del 2025
+"""
+   Una vez que conociste los tipos de datos de la categoría Collections de Python
+   Ahora la tarea consiste en declarar una variabla para cada elemento, ingresa de forma manual, al menos 30 valores para cada elemento,
+   y finalmente utiliza un for para imprimir cada uno de sus elementos en pantalla.
+   Fecha límite de entrega: Lúunes 17 de febrero del 2025
+
+   ¡Éxito en esta actividad!
+"""
