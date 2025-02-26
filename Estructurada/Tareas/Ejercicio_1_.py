91ca68b8204f881d2c8abcc437f9ff119654e944
MSC Tomás González <tomas.gonzalez@smartin.tecnm.mx>
2025-02-26 10:56:51 -0600

    Tareas 2 calificada

--- Estructurada/Tareas/Ejercicio_1_.py
+++ Estructurada/Tareas/Ejercicio_1_.py
@@ -1,23 +1,25 @@
 #crear un metodo llamado agregar, paso un valor entero, lista.appen y retornar, imprimir 10 veces el metodo
-def Lista(a:int)->list:
-    lista=[]
+"""
+ Al revisar el código observo que tienes unas pequeñas observaciones. Bien finalemnte lo he corregido para que lo revises.
+ Calificación 8
+"""
+def Lista(a,lista:list):
     lista.append(a)
-    return lista
-
 
 if __name__ == '__main__':
+    lista=[] #Comenzamos con una lista vacía.
 
-    l=Lista("10")
-    l=Lista("6")
-    l=Lista("17")
-    l=Lista("54")
-    l=Lista("15")
-    l=Lista("30")
-    l=Lista("18")
-    l=Lista(19)
-    l=Lista(48)
-    l=Lista(68)
+    Lista("10",lista) #Aprovecha la fortaleza de la referencia a objeto.
+    Lista("6",lista)
+    Lista("17",lista)
+    Lista("54",lista)
+    Lista("15",lista)
+    Lista("30",lista)
+    Lista("18",lista)
+    Lista(19,lista)
+    Lista(48,lista)
+    Lista(68,lista)
 
     print("********* Lista original *********")
-    for l in Lista:
+    for l in lista:
         print(l)
